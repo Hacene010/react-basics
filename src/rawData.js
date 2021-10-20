4dd04a2c6e386113a155fe425bf5726b1f73652c
LoicBrassart <loic.brassart@gmail.com>
2021-10-20 16:47:07 +0200

    Add dedicated data file

---
+++ src/rawData.js
@@ -0,0 +1,12 @@
+const rawData = [
+  {
+    title: 'Couscous',
+    image:
+      'https://thumbs.dreamstime.com/b/west-african-food-assortment-west-african-food-concept-traditional-wset-african-dishes-assortment-peanut-soup-jollof-rice-grilled-136692172.jpg',
+  },
+  {
+    title: 'Riz au curry',
+    image: 'https://assets.afcdn.com/recipe/20200831/113634_origin.jpg',
+  },
+];
+export default rawData;
