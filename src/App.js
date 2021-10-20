a6505fcaf367410c3a76bf9be86e9d5fb3cc9ee1
LoicBrassart <loic.brassart@gmail.com>
2021-10-20 15:59:48 +0200

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Dish from './components/Dish';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Dish
         title='Couscous'
         image='https://thumbs.dreamstime.com/b/west-african-food-assortment-west-african-food-concept-traditional-wset-african-dishes-assortment-peanut-soup-jollof-rice-grilled-136692172.jpg'
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>My awesome Recipes</h1>
+      <input type='search' />
+    </header>
+  );
+}
