f1629d0ae2a39454db705199de97ad643b01dfca
LoicBrassart <loic.brassart@gmail.com>
2021-10-20 17:32:23 +0200

    Implement basic filter bar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Dish from './components/Dish';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -6,14 +7,19 @@ import rawData from './rawData';
 import Criteria from './components/Criteria';
 
 function App() {
+  const [needle, setNeedle] = useState('');
   return (
     <div className='App'>
-      <Header />
+      <Header needle={needle} setNeedle={setNeedle} />
       <div className='gallery'>
         <main className='gallery'>
-          {rawData.map((dish) => {
-            return <Dish {...dish} />;
-          })}
+          {rawData
+            .filter((dish) => {
+              return dish.title.includes(needle);
+            })
+            .map((dish) => {
+              return <Dish {...dish} />;
+            })}
         </main>
         <Criteria />
       </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,16 @@
 import './style.css';
 
-export default function Header() {
+export default function Header({ needle, setNeedle }) {
   return (
     <header>
       <h1>My awesome Recipes</h1>
-      <input type='search' />
+      <input
+        type='search'
+        value={needle}
+        onChange={(evt) => {
+          setNeedle(evt.target.value);
+        }}
+      />
     </header>
   );
 }
